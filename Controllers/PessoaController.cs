3cce57a561cc87bd230145cba2f72954f2a82965
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-19 21:47:11 -0300

    HttpPost

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -2,12 +2,29 @@ using System.Threading.Tasks;
 using api.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using api.Models;
 namespace api.Controllers
 {
     [Controller]
-    [Route("[controller]")]
-    public class PessoaController
+    [Route("pessoa")]
+    public class PessoaController : ControllerBase
     {
+        private  DataContext dc;
+
+        public   PessoaController(DataContext context)
+        {
+            this.dc = context;
+        }
+
+
+            [HttpPost("api")]
+        public async Task <ActionResult> Cadastrar([FromBody] Pessoa p)
+        {
+            dc.pessoa.Add(p);
+            await dc.SaveChangesAsync();
+            return Created("Objeto pessoa", p);
+        }
+
         [HttpGet("oi")]
         public string oi()
         {
